bff48dcbdb390156faa1ae5822d960e60d13840c
voak-v-nandagiri <147560427+voak-v-nandagiri@users.noreply.github.com>
2023-11-15 11:08:34 +1100

    voak-gen-ai-tool-v0-b0044

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+#import streamlit as st
+#from gradio_client import Client
+
+#from flask import Flask
+#app = Flask(__name__)
+
+#@app.route("/")
+#def index():
+#    return "<h1>Voak Generative AI Tool - Landing Page</h1>"
+
 import streamlit as st
-from gradio_client import Client
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "<h1>Voak Generative AI Tool - Landing Page</h1>"
-
 from transformers import pipeline
 
 pipe = pipeline('sentiment-analysis')
